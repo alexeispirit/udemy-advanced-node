c9aa219e6fc44e7de7e8dd1cad209cd29f39d750
alexeispirit <alexeispirit@gmail.com>
2019-08-12 17:21:36 +0500

    63-toggleable-cache

--- AdvancedNodeStarter/services/cache.js
+++ AdvancedNodeStarter/services/cache.js
@@ -8,8 +8,15 @@ client.get = promisify(client.get);
 
 const exec = mongoose.Query.prototype.exec;
 
+mongoose.Query.prototype.cache = function() {
+  this.useCache = true;
+  return this;
+};
+
 mongoose.Query.prototype.exec = async function() {
-  console.log('IM ABOUT TO RUN A QUERY');
+  if (!this.useCache) {
+    return exec.apply(this, arguments);
+  }
 
   const key = JSON.stringify(
     Object.assign({}, this.getQuery(), {

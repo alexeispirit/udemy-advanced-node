a943ead5bb34a0ef8d4dafe298c1ba4c417270f9
alexeispirit <alexeispirit@gmail.com>
2019-08-12 16:54:54 +0500

    58-restore-redis-config

--- AdvancedNodeStarter/services/cache.js
+++ AdvancedNodeStarter/services/cache.js
@@ -1,13 +1,21 @@
 const mongoose = require('mongoose');
+const redis = require('redis');
+const promisify = require('util').promisify;
+
+const redisUrl = 'redis://127.0.0.1:6379';
+const client = redis.createClient(redisUrl);
+client.get = promisify(client.get);
 
 const exec = mongoose.Query.prototype.exec;
 
 mongoose.Query.prototype.exec = function() {
   console.log('IM ABOUT TO RUN A QUERY');
 
-  const key = Object.assign({}, this.getQuery(), {
-    collection: this.mongooseCollection.name
-  });
+  const key = JSON.stringify(
+    Object.assign({}, this.getQuery(), {
+      collection: this.mongooseCollection.name
+    })
+  );
 
   return exec.apply(this, arguments);
 };
